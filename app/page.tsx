8f216cb7373eca5a91fb835d1173a9ed512f9105
Paraxanos <pranjal.s24@iiits.in>
2025-09-08 19:39:45 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { InteractiveGridPatternDemo } from "../components/interactive-grid-pattern-demo-2"
+import NavbarDemo from "../components/navbar-demo"
+
+export default function SyntheticV0PageForDeployment() {
+  return (
+  <>
+  <NavbarDemo />
+  <InteractiveGridPatternDemo />
+  </>)
+}
